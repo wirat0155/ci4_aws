0b4f3daec949db15e3006978af1679ce1ca14a94
Wirat Sakorn <sunneed.2555@gmail.com>
2023-12-27 21:31:23 +0700

    Connect DB

--- src/app/Config/Database.php
+++ src/app/Config/Database.php
@@ -26,10 +26,10 @@ class Database extends Config
      */
     public array $default = [
         'DSN'          => '',
-        'hostname'     => 'localhost',
-        'username'     => '',
-        'password'     => '',
-        'database'     => '',
+        'hostname'     => 'mysqlHost',
+        'username'     => 'root',
+        'password'     => 'cod31gn1t3',
+        'database'     => 'codeigniter_db',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
--- src/app/Controllers/Home.php
+++ src/app/Controllers/Home.php
@@ -1,11 +1,22 @@
 <?php
 
 namespace App\Controllers;
+use App\Models\StudentModel;
 
 class Home extends BaseController
 {
     public function index(): string
     {
-        return view('welcome_message');
+        $studentModel = new StudentModel();
+        $data['results'] = $studentModel->findAll();
+
+        // $db = \Config\Database::connect();
+        // $result = $db->query('select * from student');
+
+        // $db = \Config\Database::connect("default");
+        // $result = $db->query('select * from student');
+        print_r($data['results']);
+
+        return view('welcome_message', $data);
     }
 }
